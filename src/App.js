6db13d5069c4610dce9e59bb57b1206db65143b4
jterroba <jterroba@gmail.com>
2021-05-23 21:33:39 +0200

    Added history and create

--- src/App.js
+++ src/App.js
@@ -27,6 +27,15 @@ class App extends Component {
     ContactsAPI.remove(contact)
   }
 
+  createContact = (contact) => {
+    ContactsAPI.create(contact)
+      .then((contact) => {
+        this.setState((currentState) => ({
+          contacts: currentState.contacts.concat([contact])
+        }))
+      })
+  }
+
   render() {
     return (
       <div>
@@ -36,8 +45,15 @@ class App extends Component {
                 onDeleteContact={this.removeContact}               
             />
         )} />
-
-        <Route exact path='/create'  component={CreateContact} />       
+        
+        <Route path='/create' render={({ history }) => (
+          <CreateContact
+            onCreateContact={(contact) => {
+              this.createContact(contact)
+              history.push('/')
+            }}
+          />
+        )} />       
       </div>
     );
   }
